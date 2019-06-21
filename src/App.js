d9e41fdc4ca7bd1b1232390951d9ee19b144de5a
kkesley <kendrick.kesley.dev@gmail.com>
2019-06-21 22:27:06 +1000

    init bulma

--- src/App.js
+++ src/App.js
@@ -5,20 +5,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-success is-link is-fullheight">
+        <div className="hero-header">
+          <div className="container">
+            <nav className="navbar is-transparent">
+              <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                  <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+                </a>
+                <div className="navbar-burger burger" data-target="navbar-main">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+
+              <div id="navbar-main" className="navbar-menu">
+                <div className="navbar-end">
+                  <a className="navbar-item" href="https://bulma.io/">
+                    Home
+                  </a>
+                  <a className="navbar-item" href="https://bulma.io/">
+                    Test
+                  </a>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Fullheight title
+            </h1>
+            <h2 className="subtitle">
+              Fullheight subtitle
+            </h2>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
