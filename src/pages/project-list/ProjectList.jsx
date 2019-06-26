9dd69618a64964d0ff86b6038ad80225eba513eb
kkesley <kendrick.kesley.dev@gmail.com>
2019-06-26 19:58:08 +1000

    add react helmet

--- src/pages/project-list/ProjectList.jsx
+++ src/pages/project-list/ProjectList.jsx
@@ -1,10 +1,21 @@
 import React, { memo } from 'react'
+import Helmet from 'react-helmet'
 import Banner from './Banner'
 import Project from './Project'
 
 const ProjectList = () => {
   return (
     <div id="page__project-list">
+        <Helmet>
+            <title>Projects - Kendrick Kesley</title>
+            <link rel="canonical" href="https://kkesley.com/projects" />
+            <meta property="og:url" content="https://kkesley.com/projects"/>
+            <meta name="description" content="These are commercial and personal projects throughout my journey." />
+            <meta property="og:description" content="These are commercial and personal projects throughout my journey." />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Projects - Kendrick Kesley" />
+            <meta property="og:image" content="https://avatars3.githubusercontent.com/u/14907878?s=40&v=4" />
+        </Helmet>
         <Banner />
         <div className="container"><hr /></div>
         <Project />
