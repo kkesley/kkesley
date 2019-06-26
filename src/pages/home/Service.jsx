50912c339956aa3965f83e6dacf8128bae020cbe
kkesley <kendrick.kesley.dev@gmail.com>
2019-06-26 20:02:59 +1000

    modify some content

--- src/pages/home/Service.jsx
+++ src/pages/home/Service.jsx
@@ -14,21 +14,21 @@ const Service = () => (
                     <div className="content is-medium">
                         <img role="presentation" alt="Interactive Application" src={ImageWeb} />
                         <h4 className="title is-4 is-spaced">Interactive Application</h4>
-                        <p className="subtitle is-5">User experience is critical to the success of an application. Efficient interaction with beautiful interface are always in my priorities.</p>
+                        <p className="subtitle is-5">User experience is critical to the success of an application. Efficient interactions with beautiful interface are always in my priorities.</p>
                     </div>
                 </div>
                 <div className="column is-one-third">
                     <div className="content is-medium">
                         <img role="presentation" alt="System Design" src={ImageSystem} />
                         <h4 className="subtitle is-4">System Design</h4>
-                        <p className="subtitle is-5">I love creating scalable system by creating components and combining them. It's always been my passion to architect systems, specifically in AWS.</p>
+                        <p className="subtitle is-5">I love creating scalable systems by creating components and combining them. It's always been my passion to architect systems, especially in AWS.</p>
                     </div>
                 </div>
                 <div className="column is-one-third">
                     <div className="content is-medium">
                         <img role="presentation" alt="Data Structure" src={ImageData} />
                         <h4 className="subtitle is-4">Data Structure</h4>
-                        <p className="subtitle is-5">Either SQL or NoSQL, I'm passionate in structuring data for a system. I believe efficient data structures can have a big impact on system qualities.</p>
+                        <p className="subtitle is-5">Either SQL or NoSQL, I'm passionate in structuring data for systems. I believe efficient data structures can have a big impact on system qualities.</p>
                     </div>
                 </div>
             </div>
