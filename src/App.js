6670eec8e48dd8491ec5d399e9ec1890a3843220
kkesley <kendrick.kesley.dev@gmail.com>
2019-06-27 21:03:10 +1000

    use react lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { ApolloProvider } from 'react-apollo'
 
@@ -9,12 +9,14 @@ import graphqlClient from 'helper/appsync-client'
 import ScrollToTop from 'components/scroll-to-top'
 import Navigation from 'components/navigation'
 import Footer from 'components/footer'
+import Spinner from 'components/spinner'
 
 import Home from 'pages/home'
-import ProjectList from 'pages/project-list'
-import ProjectDetail from 'pages/project-detail'
 
-import withTracker from 'helper/withTracker';
+import withTracker from 'helper/withTracker'
+
+const ProjectList = lazy(() => import('pages/project-list'))
+const ProjectDetail = lazy(() => import('pages/project-detail'))
 
 const App = () => (
   <ApolloProvider client={graphqlClient}>
@@ -23,11 +25,13 @@ const App = () => (
         <div className="App">
           <Navigation/>
           <div className="App-content">
-            <Switch>
-              <Route path="/" exact component={withTracker(Home)} />
-              <Route path="/projects" exact component={withTracker(ProjectList)} />
-              <Route path="/projects/:project" component={withTracker(ProjectDetail)} />
-            </Switch>
+            <Suspense fallback={<Spinner />}>
+              <Switch>
+                <Route path="/" exact component={withTracker(Home)} />
+                <Route path="/projects" exact component={withTracker(ProjectList)} />
+                <Route path="/projects/:project" component={withTracker(ProjectDetail)} />
+              </Switch>
+            </Suspense>
           </div>
           <Footer />
         </div>
