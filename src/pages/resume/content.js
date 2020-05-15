9ead172870885d83869b18d78631d707e031923f
Kendrick Kesley <kkesley@Kendricks-MacBook-Pro.local>
2020-05-15 16:27:14 +1000

    Add resume

--- src/pages/resume/content.js
+++ src/pages/resume/content.js
@@ -2,92 +2,118 @@ import ImageBinus from 'assets/university/binus-international.png'
 import ImageRMIT from 'assets/university/rmit.jpg'
 
 export default {
-    personal_detail: {
-      name: 'Kendrick Kesley',
-      job_title: 'Software Developer',
-      email: 'kendrick.kesley.dev@gmail.com',
-      location: 'Melbourne, Australia',
+  personal_detail: {
+    name: 'Kendrick Kesley',
+    job_title: 'Software Developer',
+    email: 'kendrick.kesley.dev@gmail.com',
+    location: 'Melbourne, Australia',
+  },
+  careers: [
+    {
+      title: 'Consultant',
+      company: 'Shine Solutions',
+      year: '2019 - Current',
+      contents: [
+        'Redesigned part of a system for a client using React and AWS Amplify with 4 other team members.',
+        'Extensive tests with Jest & Cypress.',
+      ],
     },
-    careers: [{
+    {
       title: 'Full-Stack Developer',
       company: 'Psych Press',
-      year: '2017 - Current',
+      year: '2017 - 2019',
       contents: [
-        'Developed back-end systems using "Serverless Architecture" using AWS, serverless framework, and multiplelanguages such as Golang and NodeJS.',
-        'Applied DevOps methods to automating building and deploying process using AWS CodeBuild, AWS S3, andGIT for both for back-end and front-end.'
+        'Developed back-end systems using "Serverless Architecture" using AWS, serverless framework, and multiple languages such as Golang and NodeJS.',
+        'Applied DevOps methods to automating building and deploying process using AWS CodeBuild, AWS S3, and GIT for both for back-end and front-end.',
       ],
-    }, {
+    },
+    {
       title: 'Full-Stack Developer',
       company: 'ITEA Solution',
       year: '2016 - 2017',
       contents: [
-        'Iteacup (made with AngularJS and PHP as the back-end). This project allows me to understand about stock management system and accounting system for businesses. This project has sensitive information so only authorized user can login.',
-        'Taxation system (made with AngularJS and Firebase as the database). This project allows me to understand about taxation process in Indonesia, how to automatically fill pdf forms by using PDFtk library, and how to deal with asynchronous requests.'
+        'Iteacup (made with AngularJS and PHP as the back-end). This project allows me to understand about stock management system and accounting system for businesses.',
+        'Taxation system (made with AngularJS and Firebase as the database). This project allows me to understand about taxation process in Indonesia.',
       ],
-    }],
-    projects: [{
+    },
+  ],
+  projects: [
+    {
       title: 'veronicaonggoro.com',
-      year: '2018',
+      year: '2020 (version 2)',
+      contents: [
+        'Repurposed old website (blogging) into a portfolio website (photography).',
+        'Used AWS MediaConvert for encoding uploaded videos to HLS format to support multiple resolutions streaming in the web.',
+        'Replaced REST API with GraphQL (using AppSync) to reduce the number of code that I have to maintain.',
+        'Introduced a client portal for customers so the business process is streamlined (and to encourage customers to give feedback in the system).',
+      ],
+    },
+    {
+      title: 'veronicaonggoro.com',
+      year: '2018 (version 1)',
       contents: [
         'Developed a personal email system using AWS SES and AWS S3.',
-        'Created both back-end and front-end using "Serverless Architecture" with AWS Lambda, AWS S3, and AWS DynamoDB.',
+        'Created both back-end using "Serverless Architecture" with API Gateway, AWS Lambda, AWS S3, and AWS DynamoDB.',
         'Front-end created with ReactJS.',
         'Managed to run a scalable website with low monthly cost.',
-        'Applied optimization using GZip, browser caching, minification, and CDN'
       ],
-    }, {
+    },
+    {
       title: 'propertianda.com',
       year: '2016',
       contents: [
         'PropertiAnda is made with AngularJS and PHP as the back-end programming language. This project allows me to understand about crowdfunding and bidding/offering system.',
-        'One of the largest systems I have built.',
       ],
-    }, {
+    },
+    {
       title: 'gakushudo.com',
       year: '2016',
       contents: [
         'The website is made with AngularJS and PHP as the back-end programming language. This project allows me to understand the basic concept of E-commerce and ordering system.',
         'Gakushudo Learning Application (made with Ionic2 and PHP as the back-end programming language) This project allows me to understand how to run online classes complete with class schedules, online quizzes,and online absence system.',
       ],
-    }],
-    skills: [{
-        section: 'Technical',
-        contents: [
-            'Passed AWS Certified Solutions Architect – Associate certification & AWS Certified Developer - Associate (2019 - 2022)',
-            'Designed Serverless Architecture to reduce cost and improve scalability using AWS ecosystem.',
-            'Developed websites and mobile applications as a full-stack development.',
-            'Fluent in front-end development using ReactJS / AngularJS.',
-            'Fluent in back-end development using PHP, NodeJS, and Golang.',
-            'Experienced in DynamoDB, MySQL, MSSQL, PostgreSQL, and MongoDB.',
-            'Programmed using MVC Frameworks e.g. Laravel, .NET CORE, Ruby on Rails, and Spring.',
-        ],
-    }, {
-        section: 'Management',
-        contents: [
-            'Managed time proportionally between work and study.',
-            'Handled tight deadlines.',
-        ],
-    }, {
-        section: 'Teamwork',
-        contents: [
-            'Worked with non-developer teams e.g. designers and business planners.',
-            'Worked with developer teams.',
-        ],
-    }],
-    educations: [{
-        university: 'RMIT University Australia',
-        year_completion: '2019',
-        title: 'Master of Information Technology',
-        image: ImageRMIT,
-    }, {
-        university: 'BINUS International University',
-        year_completion: '2018',
-        title: 'Bachelor of Computer Science',
-        image: ImageBinus,
-    }, {
-        university: 'RMIT University Australia',
-        year_completion: '2017',
-        title: 'Bachelor of Information Technology',
-        image: ImageRMIT,
-    }]
+    },
+  ],
+  skills: [
+    {
+      section: 'Technical',
+      contents: [
+        'Passed AWS Certified Solutions Architect – Associate certification & AWS Certified Developer - Associate (2019 - 2022)',
+        'Designed Serverless Architecture to reduce cost and improve scalability using AWS ecosystem.',
+        'Fluent in front-end development using ReactJS with Typescript.',
+        'Fluent in back-end development using NodeJS (Typescript)',
+        'Developed mobile applications (using React Native).',
+        'Experienced in Golang with Serverless Framework',
+        'Experienced in DynamoDB and MySQL.',
+      ],
+    },
+    {
+      section: 'Management & Teamwork',
+      contents: [
+        'Experienced in estimation and prioritisation sessions.',
+        'Worked in a Scrum-based team (daily standups, sprint plannings, and retrospective sessions)',
+        'Worked with non-developer teams e.g. designers and product owners.',
+      ],
+    },
+  ],
+  educations: [
+    {
+      university: 'RMIT University Australia (3.7 GPA)',
+      year_completion: '2019',
+      title: 'Master of Information Technology',
+      image: ImageRMIT,
+    },
+    {
+      university: 'BINUS International University (3.9 GPA)',
+      year_completion: '2018',
+      title: 'Bachelor of Computer Science',
+      image: ImageBinus,
+    },
+    {
+      university: 'RMIT University Australia (4.0 GPA)',
+      year_completion: '2017',
+      title: 'Bachelor of Information Technology',
+      image: ImageRMIT,
+    },
+  ],
 }
--- src/pages/resume/downloadResume.js
+++ src/pages/resume/downloadResume.js
@@ -1,165 +1,196 @@
-import dayjs from 'dayjs'
 import PDFMake from 'pdfmake'
-import PDFFonts from "pdfmake/build/vfs_fonts"
+import PDFFonts from 'pdfmake/build/vfs_fonts'
 
 PDFMake.vfs = PDFFonts.pdfMake.vfs
 
 const TITLE_COLOR = '#2a7a87'
 
-const horizontalLine = function(width_percent) {
-    width_percent = width_percent === undefined ? 100 : width_percent;
+const horizontalLine = function (width_percent) {
+  width_percent = width_percent === undefined ? 100 : width_percent
 
-    var length = 514 / 100 * width_percent;
+  var length = (514 / 100) * width_percent
 
-    var return_obj = {
-        "margin": [0, 5, 0, 5],
-        "canvas": [{
-            "type": "line",
-            "x1": 0,
-            "y1": 0,
-            "x2": length,
-            "y2": 0,
-            "lineWidth": 2,
-            "lineColor": TITLE_COLOR
-        }]
-    };
+  var return_obj = {
+    margin: [0, 5, 0, 5],
+    canvas: [
+      {
+        type: 'line',
+        x1: 0,
+        y1: 0,
+        x2: length,
+        y2: 0,
+        lineWidth: 2,
+        lineColor: TITLE_COLOR,
+      },
+    ],
+  }
 
-    return return_obj;
-};
+  return return_obj
+}
 
-export default ({ personal_detail, careers, projects, skills, educations }) => () => {
+export default ({
+  personal_detail,
+  careers,
+  projects,
+  skills,
+  educations,
+}) => () => {
   PDFMake.createPdf({
-    header: {
-        columns: [
-            'Resume - Kendrick Kesley',
-            { text: `generated on: ${dayjs().format('DD MMM YYYY HH:mm:ss Z')}`, alignment: 'right' }
-        ],
-        color: '#808080', //grey
-        fontSize: 8,
-        margin: [40, 12, 40, 0]
-    },
+    // header: {
+    //   columns: [
+    //     'Resume - Kendrick Kesley',
+    //     {
+    //       text: `generated on: ${dayjs().format('DD MMM YYYY HH:mm:ss Z')}`,
+    //       alignment: 'right',
+    //     },
+    //   ],
+    //   color: '#808080', //grey
+    //   fontSize: 8,
+    //   margin: [40, 12, 40, 0],
+    // },
     footer: (currentPage, pageCount) => ({
-        text: `page ${currentPage.toString()} of ${pageCount}`,
-        alignment: 'right',
-        color: '#808080', //grey
-        fontSize: 8,
-        margin: [40, 0, 40, 0],
+      text: `page ${currentPage.toString()} of ${pageCount}`,
+      alignment: 'right',
+      color: '#808080', //grey
+      fontSize: 8,
+      margin: [40, 0, 40, 0],
     }),
     content: [
-        //START: TITLE
-        {
-            stack: [
-                { text: personal_detail.name, style: 'intro' },
-                horizontalLine(),
-                {
-                    text: [
-                        personal_detail.location,
-                        ' | ',
-                        { text: personal_detail.email, link: `mailto:${personal_detail.email}`, style: 'link' },
-                        ' | ',
-                        { text: 'https://kkesley.com', link: 'https://kkesley.com', style: 'link' },
-                    ],
-                },
+      //START: TITLE
+      {
+        stack: [
+          { text: personal_detail.name, style: 'intro' },
+          horizontalLine(),
+          {
+            text: [
+              personal_detail.location,
+              ' | ',
+              {
+                text: personal_detail.email,
+                link: `mailto:${personal_detail.email}`,
+                style: 'link',
+              },
+              ' | ',
+              {
+                text: 'https://kkesley.com',
+                link: 'https://kkesley.com',
+                style: 'link',
+              },
+              ' | ',
+              {
+                text: 'tel: 0416842836',
+              },
             ],
-        },
-        //END: TITLE
-        //START: CAREER
-        {
+          },
+        ],
+      },
+      //END: TITLE
+      //START: CAREER
+      {
+        stack: [
+          { text: 'Career', style: 'header' },
+          careers.map((career) => ({
             stack: [
-                { text: 'Career', style: 'header' },
-                careers.map(career => ({
-                    stack: [
-                        { text: `${career.title} | ${career.company} | ${career.year}`, style: 'item_title' },
-                        {
-                            ul: career.contents.map(content => content)
-                        }
-                    ],
-                    margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in careers
-                }))
+              {
+                text: `${career.title} | ${career.company} | ${career.year}`,
+                style: 'item_title',
+              },
+              {
+                ul: career.contents.map((content) => content),
+              },
             ],
-            margin: [0, 12, 0, 0], //TOP: 12 | for this section
-        },
-        //END: CAREER
-        //START: PROJECTS
-        {
+            margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in careers
+          })),
+        ],
+        margin: [0, 12, 0, 0], //TOP: 12 | for this section
+      },
+      //END: CAREER
+      //START: PROJECTS
+      {
+        stack: [
+          { text: 'Projects', style: 'header' },
+          projects.map((project) => ({
             stack: [
-                { text: 'Projects', style: 'header' },
-                projects.map(project => ({
-                    stack: [
-                        { text: `${project.title} | ${project.year}`, style: 'item_title' },
-                        {
-                            ul: project.contents.map(content => content)
-                        }
-                    ],
-                    margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in projects
-                }))
+              {
+                text: `${project.title} | ${project.year}`,
+                style: 'item_title',
+              },
+              {
+                ul: project.contents.map((content) => content),
+              },
             ],
-            margin: [0, 12, 0, 0], //TOP: 12 | for this section
-        },
-        //END: PROJECTS
-        //START: SKILLS
-        {
+            margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in projects
+          })),
+        ],
+        margin: [0, 12, 0, 0], //TOP: 12 | for this section
+      },
+      //END: PROJECTS
+      //START: SKILLS
+      {
+        stack: [
+          { text: 'Skills', style: 'header' },
+          skills.map((skill) => ({
             stack: [
-                { text: 'Skills', style: 'header' },
-                skills.map(skill => ({
-                    stack: [
-                        { text: skill.section, style: 'item_title' },
-                        {
-                            ul: skill.contents.map(content => content)
-                        }
-                    ],
-                    margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in skills
-                }))
+              { text: skill.section, style: 'item_title' },
+              {
+                ul: skill.contents.map((content) => content),
+              },
             ],
-            margin: [0, 12, 0, 0], //TOP: 12 | for this section
-        },
-        //END: SKILLS
-        //START: EDUCATION
-        {
+            margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in skills
+          })),
+        ],
+        margin: [0, 12, 0, 0], //TOP: 12 | for this section
+      },
+      //END: SKILLS
+      //START: EDUCATION
+      {
+        stack: [
+          { text: 'Education', style: 'header' },
+          educations.map((education) => ({
             stack: [
-                { text: 'Education', style: 'header' },
-                educations.map(education => ({
-                    stack: [
-                        { text: `${education.title} | ${education.year_completion}`, style: 'item_title' },
-                        { text: education.university },
-                    ],
-                    margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in education
-                }))
+              {
+                text: `${education.title} | ${education.year_completion}`,
+                style: 'item_title',
+              },
+              { text: education.university },
             ],
-            margin: [0, 12, 0, 0], //TOP: 12 | for this section
-        },
-        //END: EDUCATION
+            margin: [0, 0, 0, 12], //BOTTOM: 12 | for every item in education
+          })),
+        ],
+        margin: [0, 12, 0, 0], //TOP: 12 | for this section
+      },
+      //END: EDUCATION
     ],
     defaultStyle: {
-        lineHeight: 1.5,
-        fontSize: 11,
+      lineHeight: 1.5,
+      fontSize: 11,
     },
     styles: {
-        intro: {
-            color: TITLE_COLOR,
-            fontSize: 25,
-            lineHeight: 1,
-        },
-        header: {
-            color: TITLE_COLOR,
-            fontSize: 16,
-            bold: true,
-        },
-        item_title: {
-            color: '#000000',
-            fontSize: 16,
-            bold: true,
-        },
-        link: {
-            color: TITLE_COLOR,
-            decoration: 'underline',
-        }
+      intro: {
+        color: TITLE_COLOR,
+        fontSize: 25,
+        lineHeight: 1,
+      },
+      header: {
+        color: TITLE_COLOR,
+        fontSize: 16,
+        bold: true,
+      },
+      item_title: {
+        color: '#000000',
+        fontSize: 16,
+        bold: true,
+      },
+      link: {
+        color: TITLE_COLOR,
+        decoration: 'underline',
+      },
     },
     info: {
-        title: 'Resume - Kendrick Kesley',
-        author: 'Kendrick Kesley',
-        subject: 'Resume',
-        keywords: 'resume kendrick kesley',
+      title: 'Resume - Kendrick Kesley',
+      author: 'Kendrick Kesley',
+      subject: 'Resume',
+      keywords: 'resume kendrick kesley',
     },
   }).download('Resume - Kendrick Kesley')
 }
